b19e6777737cd0c63e05e579f8ac57361f014aeb
Nestor_Leiva <nestorjoel2@gmail.com>
2023-07-19 20:59:18 -0600

    add validaciones frmConversorDivisa

---
+++ src/Logica/ConversorDivisas.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Logica;
+import java.awt.event.KeyEvent;
+/**
+ *
+ * @author nestor
+ */
+
+public class ConversorDivisas extends GUI.frmConversorDivisa{
+    
+    
+
+    
+}
